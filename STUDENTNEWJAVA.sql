04e6ea80548c55c24d4a28012ef0244123eaf663
G Durgaprasad <durgaprasadgadiparthi4@gmail.com>
2025-08-07 10:56:31 +0530

    Add files via upload

---
+++ STUDENTNEWJAVA.sql
@@ -0,0 +1,9 @@
+create database mec;
+use mec;
+create table student(
+id int auto_increment,
+name varchar(40) not null,
+Fee varchar(20) not null,
+primary key(id));
+Drop table student;
+select * from student;
